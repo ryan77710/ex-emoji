41ed9a9203f729119c215c73cdd5be3040867914
lollia ryan <lolliaryan@ryan-ordi.local>
2021-06-18 02:11:58 +0200

    add pop up when user click to copy

--- src/App.js
+++ src/App.js
@@ -4,12 +4,26 @@ import Search from "./Components/Search/Search";
 import Line from "./Components/Line/Line";
 import Footer from "./Components/Footer/Footer";
 
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 import { useState } from "react";
 
 function App() {
   const [input, setInput] = useState("");
   return (
     <div className="App">
+      <ToastContainer
+        position="bottom-center"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
       <Search input={input} setInput={setInput}></Search>
       <Line input={input}></Line>
       <Footer></Footer>
--- src/Components/Line/DivEmoji.js
+++ src/Components/Line/DivEmoji.js
@@ -1,10 +1,15 @@
 import React from "react";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 
+import { toast } from "react-toastify";
+
 const DivEmoji = (props) => {
   return (
     <CopyToClipboard text={props.symbol}>
-      <div onClick={() => {}} className="DivEmoji">
+      <div
+        onClick={() => toast.warning(` emojie ${props.symbol} copied`)}
+        className="DivEmoji"
+      >
         <div>
           <span>{props.symbol}</span>
           <span>{props.title}</span>
